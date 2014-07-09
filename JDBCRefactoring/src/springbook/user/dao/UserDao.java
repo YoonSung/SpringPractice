8c46d45b9e8f52ff467f5ca85cabaef679c74b58
Yoonsung <lvev9925@naver.com>
2014-07-09 21:21:51 +0900

    초난감 JDBC 기본 소스코드 생성

---
+++ JDBCRefactoring/src/springbook/user/dao/UserDao.java
@@ -0,0 +1,48 @@
+package springbook.user.dao;
+import java.sql.Connection;
+import java.sql.DriverManager;
+import java.sql.PreparedStatement;
+import java.sql.ResultSet;
+import java.sql.SQLException;
+
+import springbook.user.domain.User;
+
+public class UserDao {
+	
+	public void add(User user) throws SQLException, ClassNotFoundException {
+		Class.forName("com.mysql.jdbc.Driver");
+		Connection connection = DriverManager.getConnection("jdbc:mysql://54.178.137.153:3306/springPractice", "yoon", "spring");
+		
+		PreparedStatement preparedStatement = connection.prepareStatement("insert into users(id, name, password) values(?, ?, ?)");
+		preparedStatement.setString(1,  user.getId());
+		preparedStatement.setString(2, user.getName());
+		preparedStatement.setString(3, user.getPassword());
+		
+		preparedStatement.executeUpdate();
+		
+		preparedStatement.close();
+		connection.close();
+	}
+	
+	public User get(String id) throws ClassNotFoundException, SQLException {
+		Class.forName("com.mysql.jdbc.Driver");
+		Connection connection = DriverManager.getConnection("jdbc:mysql://54.178.137.153:3306/springPractice", "yoon", "spring");
+		
+		PreparedStatement preparedStatement = connection.prepareStatement("select * from users where id = ?");
+		
+		preparedStatement.setString(1,  id);
+		
+		ResultSet resultSet = preparedStatement.executeQuery();
+		resultSet.next();
+		User user = new User();
+		user.setId(resultSet.getString("id"));
+		user.setName(resultSet.getString("name"));
+		user.setPassword(resultSet.getString("password"));
+		
+		resultSet.close();
+		preparedStatement.close();
+		connection.close();
+		
+		return user;
+	}
+}
