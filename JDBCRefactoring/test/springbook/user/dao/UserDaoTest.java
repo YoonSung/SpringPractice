e22618c0ba97b48d475dfc514ef833f2096bf8e9
Yoonsung <lvev9925@naver.com>
2014-07-17 17:55:21 +0900

    Transfer to Spring DI

--- JDBCRefactoring/test/springbook/user/dao/UserDaoTest.java
+++ JDBCRefactoring/test/springbook/user/dao/UserDaoTest.java
@@ -2,12 +2,17 @@ package springbook.user.dao;
 
 import java.sql.SQLException;
 
+import org.springframework.context.ApplicationContext;
+import org.springframework.context.annotation.AnnotationConfigApplicationContext;
+
 import springbook.user.domain.User;
 
 
 public class UserDaoTest {
 	public static void main(String[] args) throws ClassNotFoundException, SQLException {
-		UserDao dao = new DaoFactory().userDao();
+		//UserDao dao = new DaoFactory().userDao();
+		ApplicationContext context = new AnnotationConfigApplicationContext(DaoFactory.class);
+		UserDao dao = context.getBean("userDao", UserDao.class);
 		User user = new User();
 		user.setId("lvev9925a");
 		user.setName("Jung");
