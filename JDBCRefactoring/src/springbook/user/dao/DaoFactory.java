da532a5dd802246bd0a24f0981c83a2f47e53b1d
Yoonsung <lvev9925@naver.com>
2014-07-20 15:09:22 +0900

    1장 마지막 소스코드만을 남기고 모두 삭제. 불필요한 주석 및 파일제거

--- JDBCRefactoring/src/springbook/user/dao/DaoFactory.java
+++ JDBCRefactoring/src/springbook/user/dao/DaoFactory.java
@@ -1,29 +1,17 @@
 package springbook.user.dao;
 
-import java.sql.Connection;
-import java.sql.DriverManager;
-
-import javax.sql.DataSource;
-
-import org.springframework.context.ApplicationContext;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.context.support.ClassPathXmlApplicationContext;
-import org.springframework.jdbc.datasource.SimpleDriverDataSource;
 
 @Configuration //애플리케이션 컨텍스트 또는 빈 팩토리가 사용할 설정정보라는 표시
 public class DaoFactory {
 	
 	@Bean //오브젝트 생성을 담당하는 IOC용 메소드라는 표시
 	public UserDao userDao() {
-		//의존성 주입방식
-		//return new UserDao(connectionMaker());
-		
-//		DaoFactory방식 1. (생성자)
-		//return new UserDao();
-		
-		ApplicationContext context = new ClassPathXmlApplicationContext("classpath:/applicationContext.xml");
+		ClassPathXmlApplicationContext context = new ClassPathXmlApplicationContext("classpath:/applicationContext.xml");
 		UserDao userDao = context.getBean("userDao", UserDao.class);
+		context.close();
 		return userDao;
 	}
 }
--- JDBCRefactoring/test/springbook/user/dao/UserDaoTest.java
+++ JDBCRefactoring/test/springbook/user/dao/UserDaoTest.java
@@ -2,24 +2,11 @@ package springbook.user.dao;
 
 import java.sql.SQLException;
 
-import org.springframework.context.ApplicationContext;
-import org.springframework.context.annotation.AnnotationConfigApplicationContext;
-import org.springframework.context.support.ClassPathXmlApplicationContext;
-import org.springframework.context.support.GenericXmlApplicationContext;
-
 import springbook.user.domain.User;
 
 
 public class UserDaoTest {
 	public static void main(String[] args) throws ClassNotFoundException, SQLException {
-		//UserDao dao = new DaoFactory().userDao();
-		//ApplicationContext context = new AnnotationConfigApplicationContext(DaoFactory.class);
-		//UserDao dao = context.getBean("userDao", UserDao.class);
-		
-		//ApplicationContext context = new GenericXmlApplicationContext("./resources/applicationContext.xml");
-		//ApplicationContext context = new ClassPathXmlApplicationContext("classpath:/applicationContext.xml");
-		//UserDao dao = context.getBean("userDao", UserDao.class);
-		
 		DaoFactory daoFactory = new DaoFactory();
 		UserDao dao = daoFactory.userDao();
 		
