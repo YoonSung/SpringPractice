c3cca3e9bed7dd58c09aaf29d57f33fdc2c4f029
Yoonsung <lvev9925@naver.com>
2014-07-20 17:42:14 +0900

    (2.4.1) 테스트 클래스에서 Context를 공유하는 형태로 변경 (annotation)

--- JDBCRefactoring/test/springbook/user/dao/UserDaoTest.java
+++ JDBCRefactoring/test/springbook/user/dao/UserDaoTest.java
@@ -7,13 +7,22 @@ import static org.hamcrest.CoreMatchers.*;
 
 import org.junit.Before;
 import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.context.ApplicationContext;
 import org.springframework.context.support.ClassPathXmlApplicationContext;
 import org.springframework.dao.EmptyResultDataAccessException;
+import org.springframework.test.context.ContextConfiguration;
+import org.springframework.test.context.junit4.SpringJUnit4ClassRunner;
 
 import springbook.user.domain.User;
 
+@RunWith(SpringJUnit4ClassRunner.class)
+@ContextConfiguration("classpath:/applicationContext.xml")
 public class UserDaoTest {
+	
+	@Autowired//변수타입과 일치하는 컨텍스트 내의 빈을 찾는다. ApplicationContext는 자동으로 bean에 추가된다.
+	private ApplicationContext context;
 	
 	private UserDao dao;
 	private User user1;
@@ -22,8 +31,7 @@ public class UserDaoTest {
 	
 	@Before
 	public void setUp() {
-		ApplicationContext context = new ClassPathXmlApplicationContext("classpath:/applicationContext.xml");
-		dao = context.getBean("userDao", UserDao.class);
+		dao = this.context.getBean("userDao", UserDao.class);
 		user1 = new User("lvev99251", "정윤성1", "윤성1");
 		user2 = new User("lvev99252", "정윤성2", "윤성2");
 		user3 = new User("lvev99253", "정윤성3", "윤성3");
