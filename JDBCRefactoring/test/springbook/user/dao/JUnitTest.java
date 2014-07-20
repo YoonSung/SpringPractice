32f2143120f4d6b64e0c43a46fd2b2effa513116
Yoonsung <lvev9925@naver.com>
2014-07-20 18:12:52 +0900

    (2.5.2) 리팩토링

--- JDBCRefactoring/test/springbook/user/dao/JUnitTest.java
+++ JDBCRefactoring/test/springbook/user/dao/JUnitTest.java
@@ -2,27 +2,31 @@ package springbook.user.dao;
 
 import static org.junit.Assert.*;
 import static org.hamcrest.CoreMatchers.*;
+
+import java.util.HashSet;
+import java.util.Set;
+
 import org.junit.Test;
 
 public class JUnitTest {
 	
-	static JUnitTest testObject;
+	static Set<JUnitTest> testObjects = new HashSet<JUnitTest>();
 	
 	@Test
 	public void test1() {
-		assertThat(this, is(not(sameInstance(testObject))));
-		testObject = this;
+		assertThat(testObjects, not(hasItem(this)));
+		testObjects.add(this);
 	}
 
 	@Test
 	public void test2() {
-		assertThat(this, is(not(sameInstance(testObject))));
-		testObject = this;
+		assertThat(testObjects, not(hasItem(this)));
+		testObjects.add(this);
 	}
 	
 	@Test
 	public void test3() {
-		assertThat(this, is(not(sameInstance(testObject))));
-		testObject = this;
+		assertThat(testObjects, not(hasItem(this)));
+		testObjects.add(this);
 	}
 }
