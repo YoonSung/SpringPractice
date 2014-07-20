a4c3fdf53640f57fdada3c3ed9d6f8b4e18127ed
Yoonsung <lvev9925@naver.com>
2014-07-20 16:55:25 +0900

    (2.3.3) count() 테스트 추가.

--- JDBCRefactoring/test/springbook/user/dao/UserDaoTest.java
+++ JDBCRefactoring/test/springbook/user/dao/UserDaoTest.java
@@ -30,4 +30,26 @@ public class UserDaoTest {
 		assertThat(insertUser.getName(), is(selectUser.getName()));
 		assertThat(insertUser.getPassword(), is(selectUser.getPassword()));
 	}
+	
+	@Test
+	public void count() throws SQLException {
+		DaoFactory daoFactory = new DaoFactory();
+		UserDao dao = daoFactory.userDao();
+		
+		User user1 = new User("lvev99251", "정윤성1", "윤성1");
+		User user2 = new User("lvev99252", "정윤성2", "윤성2");
+		User user3 = new User("lvev99253", "정윤성3", "윤성3");
+		
+		dao.deleteAll();
+		assertThat(dao.getCount(), is(0));
+		
+		dao.add(user1);
+		assertThat(dao.getCount(), is(1));
+		
+		dao.add(user2);
+		assertThat(dao.getCount(), is(2));
+		
+		dao.add(user3);
+		assertThat(dao.getCount(), is(3));
+	}
 }
