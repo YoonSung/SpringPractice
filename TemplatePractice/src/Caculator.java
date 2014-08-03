f5d36499d144e056db3bfdd2de8f20c0d8a0861d
Yoonsung <lvev9925@naver.com>
2014-08-03 21:20:03 +0900

    (간단한 템플릿/콜백 예제) 예외처리 및 리팩토링

--- TemplatePractice/src/Caculator.java
+++ TemplatePractice/src/Caculator.java
@@ -4,17 +4,31 @@ import java.io.IOException;
 
 public class Caculator {
 
-	public Integer calcSum(String filePath) throws NumberFormatException, IOException {
-		BufferedReader br = new BufferedReader(new FileReader(filePath));
-		Integer sum = 0;
+	public Integer calcSum(String filePath) throws IOException {
 		
-		String line = null;
-		
-		while((line = br.readLine()) != null) {
-			sum += Integer.valueOf(line);
+		BufferedReader br = null;
+		try {
+			br = new BufferedReader(new FileReader(filePath));
+			Integer sum = 0;
+			String line = null;
+			
+			while((line = br.readLine()) != null) {
+				sum += Integer.valueOf(line);
+			}
+			
+			return sum;
+		} catch (IOException e) {
+			System.out.println(e.getMessage());
+			throw e;
+		} finally {
+			
+			if (br != null) {
+				try {
+					br.close();
+				} catch (IOException e) {
+					System.out.println(e.getMessage());
+				}
+			}
 		}
-		
-		br.close();
-		return sum;
 	}
 }
