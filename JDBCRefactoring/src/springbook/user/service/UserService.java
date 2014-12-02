b821ab78dd184edda3e2ac31ac2ad3bd30f4702d
Yoonsung <lvev9925@naver.com>
2014-12-03 02:52:53 +0900

    (5.4.3) 테스트를 위한 서비스 추상화. Spring에서 제공하는 MailSender방식코드 적용

--- JDBCRefactoring/src/springbook/user/service/UserService.java
+++ JDBCRefactoring/src/springbook/user/service/UserService.java
@@ -2,16 +2,9 @@ package springbook.user.service;
 
 import java.sql.SQLException;
 import java.util.List;
-import java.util.Properties;
 
-import javax.mail.Message;
-import javax.mail.MessagingException;
-import javax.mail.Session;
-import javax.mail.Transport;
-import javax.mail.internet.AddressException;
-import javax.mail.internet.InternetAddress;
-import javax.mail.internet.MimeMessage;
-
+import org.springframework.mail.SimpleMailMessage;
+import org.springframework.mail.javamail.JavaMailSenderImpl;
 import org.springframework.transaction.PlatformTransactionManager;
 import org.springframework.transaction.TransactionStatus;
 import org.springframework.transaction.support.DefaultTransactionDefinition;
@@ -71,22 +64,20 @@ public class UserService {
 	}
 
 	private void sendUpgradeEmail(User user) {
-		Properties properties = new Properties();
-		properties.put("mail.smtp.host", "mail.ksug.org");
-		Session session = Session.getInstance(properties, null);
 		
-		MimeMessage message = new MimeMessage(session);
+		//MailSender 구현 클래스의 오브젝트를 생성
+		JavaMailSenderImpl mailSender = new JavaMailSenderImpl();
+		mailSender.setHost("mail.server.com");
 		
-			try {
-				message.setFrom(new InternetAddress("useradmin@ksug.org"));
-				message.addRecipient(Message.RecipientType.TO,  new InternetAddress(user.getEmail()));
-				message.setSubject("Upgrade 안내");
-				message.setText("사용자님의 등급이 "+user.getLevel().name() + " 로 업그레이드되었습니다.");
-				
-				Transport.send(message);
-			} catch (MessagingException e) {
-				throw new RuntimeException(e);
-			}
+		SimpleMailMessage mailMessage = new SimpleMailMessage();
+		mailMessage.setTo(user.getEmail());
+		mailMessage.setFrom("useradmin@ksug.org");
+		mailMessage.setSubject("Upgrade 안내");
+		
+		//MailMessage 인터페이스의 구현 클래스 오브젝트를 만들어 메일 내용을 작성한다
+		mailMessage.setText("사용자님의 등급이 " + user.getLevel().name());
+		
+		mailSender.send(mailMessage);
 	}
 
 	private boolean canUpgradeLevel(User user) {
