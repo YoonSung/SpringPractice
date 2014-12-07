e101b0aa6bb36c7e12bbfa5a8bcdde0949974461
Yoonsung <lvev9925@naver.com>
2014-12-07 17:07:17 +0900

    (6.4.1) 포인트컷에 대한 학습테스트

--- JDBCRefactoring/test/springbook/learningtest/jdk/ReflectionTest.java
+++ JDBCRefactoring/test/springbook/learningtest/jdk/ReflectionTest.java
@@ -7,6 +7,8 @@ import java.lang.reflect.Proxy;
 
 import org.junit.Test;
 import org.springframework.aop.framework.ProxyFactoryBean;
+import org.springframework.aop.support.DefaultPointcutAdvisor;
+import org.springframework.aop.support.NameMatchMethodPointcut;
 
 import static org.hamcrest.CoreMatchers.*;
 
@@ -59,5 +61,31 @@ public class ReflectionTest {
 		assertThat(proxiedHello.sayHello("Yoon"), is("HELLO YOON"));
 		assertThat(proxiedHello.sayHi("Yoon"), is("HI YOON"));
 		assertThat(proxiedHello.sayThankYou("Yoon"), is("THANK YOU YOON"));
+	}
+	
+	@Test
+	public void pointcutAdvisor() throws Exception {
+		ProxyFactoryBean pfBean = new ProxyFactoryBean();
+		pfBean.setTarget(new HelloTarget());
+		
+		/*
+		 * 메소드 이름을 비교해서 대상을 선정하는
+		 * 알고리즘을 제공하는 포인트컷 생성
+		 */
+		NameMatchMethodPointcut pointcut = new NameMatchMethodPointcut();
+		/*
+		 * 이름 비교조건 설정, sayH로 시작하는 모든 메소드를 선택하게 한다.
+		 */
+		pointcut.setMappedName("sayH*");
+		
+		// 포인트컷과 어드바이스를 Advisor로 묶어서 한번에 추가
+		pfBean.addAdvisor(new DefaultPointcutAdvisor(pointcut, new UppercaseAdvice()));
+		
+		Hello proxiedHello = (Hello) pfBean.getObject();
+		assertThat(proxiedHello.sayHello("Yoon"), is("HELLO YOON"));
+		assertThat(proxiedHello.sayHi("Yoon"), is("HI YOON"));
+		
+		// 메소드 이름이 포인트컷의 선정조건에 맞지 않으므로, 부가기능이 적용되지 않는다. 
+		assertThat(proxiedHello.sayThankYou("Yoon"), is("Thank you YOON"));
 	}
 }
